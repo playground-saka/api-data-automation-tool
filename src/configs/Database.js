83e4faefa1514f4d65b45b7c2ae87a4759aeeede
weexdayend <weexdayend@gmail.com>
2024-09-04 00:10:01 +0700

    set:default port, config workflows, pointing into playground

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -2,16 +2,30 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set.");
+}
+
+// playground
+const Database = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    schema: "public",
+  },
+});
+
 // localhost
-const Database = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+// const Database = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//   }
+// );
 
 // staging
 // const DATABASE_URL = process.env.DATABASE_URL;
