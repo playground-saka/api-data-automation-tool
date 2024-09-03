98ce1e6f1924cbb9334a648986dc67a1e6dddf6d
Rafijohari18 <rafijohari1812@gmail.com>
2024-09-03 21:20:29 +0700

    fix: add config mocha in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ export default [
   {
     languageOptions: {
       globals: {
+        ...globals.mocha, // Add Mocha globals like `describe`, `it`, etc.
         ...globals.browser, // Browser globals like `window`, `document`, etc.
         ...globals.node,    // Node.js globals like `process`, `__dirname`, etc.
       },
